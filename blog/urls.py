d048afb5ca9ab8a48393571b6f1268d1279273e0
[Thinh-Ngo] <[thinhnt@mobile-id.vn]>
2021-05-20 17:54:16 +0700

    fix comment count

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import (index, post, blog, search,
-                    update_post, delete_post, create_post, profile,
+from .views import (index, post, blog, search, create_post, profile,
                     PostDeleteView, PostUpdateView,
                     category_posts_view)
 
@@ -9,8 +8,6 @@ urlpatterns = [
     path("profile/", profile, name="profile"),
     path("blog/", blog, name="blog"),
     path("post/<id>/", post, name="detail-post"),
-    path("post/<id>/update", update_post, name="update-post"),
-    path("post/<id>/delete", delete_post, name="delete-post"),
     path("search/", search, name="search"),
     path("create/", create_post, name="create"),
     path("category/<category>", category_posts_view, name="category-posts"),
